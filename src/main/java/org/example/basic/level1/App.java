aaee34bbc1142aed372e2433096e91bfb3fb4417
NewJKH <newsun0805@gmail.com>
2025-04-24 14:44:47 +0900

    스캐너 검증 추가

---
+++ src/main/java/org/example/basic/level1/App.java
@@ -0,0 +1,10 @@
+package org.example.basic.level1;
+
+import org.example.core.verification.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        String str = Scanner.input(String.class);
+
+    }
+}
---
+++ src/main/java/org/example/core/excepiton/NotMatchedClass.java
@@ -0,0 +1,7 @@
+package org.example.core.excepiton;
+
+public class NotMatchedClass extends RuntimeException {
+    public NotMatchedClass(String message) {
+        super(message);
+    }
+}
